a7966cff918998cd8f56e3462474e5e18ae93464
James Kon <konjames@gmail.com>
2018-11-24 15:27:01 -0600

    adding lab3 version of bp.h

--- src/bp.h
+++ src/bp.h
@@ -8,11 +8,15 @@
 #define _BP_H_
 #define BTB_SIZE 1024
 #define PHT_SIZE 256
+#define CONDITIONAL 1
+#define UNCONDITIONAL 0
+#define VALID 1
+#define INVALID 0
 #include <inttypes.h>
 
 typedef struct 
 {
-	char GHR; /* 8 bits */
+	unsigned char GHR; /* 8 bits */
 	int PHT[PHT_SIZE]; /* each value will be in range [0,3], representing one of the 4 possible states */
 } gshare_t;
 
@@ -20,7 +24,7 @@ typedef struct
 {
 	uint64_t address_tag;
 	int valid;
-	int unconditional;
+	int conditional;
 	uint64_t branch_target;
 } BTB_entry_t;
 
@@ -32,6 +36,13 @@ typedef struct
  
 void bp_predict(/*......*/);
 void bp_update(/*......*/);
+void evaluate_prediction();
+uint32_t get_BTB_index();
+uint32_t get_8_pc_bits();
+int should_take_branch();
 
-
+/* Printing Functions */
+void print_BTB(bp_t BP);
+void print_GShare(bp_t BP);
+void print_bp(bp_t BP);
 #endif
