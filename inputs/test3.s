d342c43e304af85254fa954e4680da977156e5be
James Kon <konjames@gmail.com>
2018-11-29 15:50:57 -0600

    adding new version of test3.s

--- inputs/test3.s
+++ inputs/test3.s
@@ -2,16 +2,18 @@
 
 mov x1, 0x1000
 mov x4, 0xFFFF
-mov x5, 0x9999
+lsl x5, x4, 16
+add x5, x4, x5
+
 mov x6, 0x8888
 mov x7, 0x7777
 lsl x1, x1, 16
 mov x8, 0x3333
 mov x9, 0x4444
 
-stur x4, [x1, 0x1]
-# stur x5, [x1, 0x5]
-# stur x6, [x1, 0x9]
+stur x5, [x1, 0x1]
+# stur x5, [x1, 0x2]
+# stur x6, [x1, 0x]
 # stur x7, [x1, 0x11]
 
 ldur x10, [x1, 0x1]
